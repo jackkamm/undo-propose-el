5f1fa99a04369a959aad01b476fe4f34229f28cd
Jack Kamm <jackkamm@gmail.com>
2019-04-08 23:36:24 -0700

    Add undo-propose-commit, undo-propose-commit-buffer-only

--- undo-propose.el
+++ undo-propose.el
@@ -2,7 +2,7 @@
 
 ;; Author: Jack Kamm
 ;; Maintainer: Jack Kamm
-;; Version: 1.0.0
+;; Version: 2.0.0
 ;; Package-Requires: ((emacs "24.3"))
 ;; Homepage: https://github.com/jackkamm/undo-propose.el
 ;; Keywords: convenience, files, undo, redo, history
@@ -33,8 +33,11 @@
 ;; To use undo-propose, call "M-x undo-propose" in the buffer you are editing.
 ;; This will send you to a new temporary buffer, which is read-only except
 ;; for allowing `undo' commands.  Cycle through the list of undo's as normal.
-;; When you are finished, type "C-c C-c" to add the chain of undo's as a
-;; single edit to the undo history.  To cancel, type "C-c C-k".  You can also
+;; When you are finished, type "C-c C-c" to commit the chain of undo's.
+;; This copies both the buffer and undo-ring back to the parent buffer.
+;; Alternatively, type "C-c C-b" to copy the buffer but not the undo-ring
+;; (the changes are added as a single edit in the undo-history).
+;; To cancel, type "C-c C-k".  You can also
 ;; ediff the proposed chain of undo's by typing "C-c C-d".
 
 ;;; Code:
@@ -47,8 +50,8 @@
 \\<undo-propose-mode-map>
 Copies 'current-buffer' and 'buffer-undo-list' to a new temporary buffer,
 which is read-only except for undo commands.  After finished undoing, type
-\\[undo-propose-finish] to add the chain of undos as a single edit to the original
-buffer and its 'buffer-undo-list'.  To cancel, type \\[undo-propose-cancel], and
+\\[undo-propose-commit] to accept the chain of undos,
+or \\[undo-propose-commit-buffer-only] to copy the buffer but squash the undo's into a single edit event event.  To cancel, type \\[undo-propose-cancel], and
 to view an ediff type \\[undo-propose-diff]."
   (interactive)
   (let ((mode major-mode)
@@ -68,12 +71,13 @@ to view an ediff type \\[undo-propose-diff]."
     (setq-local buffer-read-only t)
     (setq-local undo-propose-parent orig-buffer)
     (undo-propose-mode 1)
-    (message "Undo-Propose: C-c C-c to commit, C-c C-k to cancel, C-c C-d to diff")))
+    (message "Undo-Propose: C-c C-c to commit buffer & undo-ring, C-c C-b to commit buffer only, C-c C-k to cancel, C-c C-d to diff")))
 
 (define-minor-mode undo-propose-mode
   "Minor mode for `undo-propose'."
   nil " UndoP" (make-sparse-keymap))
-(define-key undo-propose-mode-map (kbd "C-c C-c") 'undo-propose-finish)
+(define-key undo-propose-mode-map (kbd "C-c C-c") 'undo-propose-commit)
+(define-key undo-propose-mode-map (kbd "C-c C-b") 'undo-propose-commit-buffer-only)
 (define-key undo-propose-mode-map (kbd "C-c C-d") 'undo-propose-diff)
 (define-key undo-propose-mode-map (kbd "C-c C-k") 'undo-propose-cancel)
 
@@ -88,9 +92,26 @@ to view an ediff type \\[undo-propose-diff]."
 (undo-propose-wrap undo)
 (undo-propose-wrap undo-only)
 
-(defun undo-propose-finish ()
+(defun undo-propose-commit ()
+  "Quit and copy ‘undo-propose’ buffer and undo-ring back to the parent buffer."
+  (interactive)
+  (let ((tmp-buffer (current-buffer))
+        (orig-buffer undo-propose-parent)
+        (list-copy (undo-copy-list buffer-undo-list))
+        (pos (point))
+        (win-start (window-start)))
+    (copy-to-buffer orig-buffer 1 (buffer-end 1))
+    (with-current-buffer orig-buffer
+      (setq-local buffer-undo-list list-copy))
+    (kill-buffer tmp-buffer)
+    (goto-char pos)
+    (set-window-start (selected-window) win-start)
+    (message "Commit Undo-Propose!")))
+
+(defun undo-propose-commit-buffer-only ()
   "Copy ‘undo-propose’ buffer back to the parent buffer, then kill it.
-This change is added as a single edit in the undo history."
+This change is added as a single edit in the undo history (i.e., the undo-ring
+is NOT copied to the parent buffer's undo-ring)."
   (interactive)
   (let* ((tmp-buffer (current-buffer))
          (tmp-end (1+ (buffer-size tmp-buffer)))
@@ -107,6 +128,9 @@ This change is added as a single edit in the undo history."
         (goto-char first-diff)))
     (kill-buffer tmp-buffer)
     (message "Commit Undo-Propose!")))
+
+(define-obsolete-function-alias 'undo-propose-finish
+  'undo-propose-commit-buffer-only "2.0.0")
 
 (defun undo-propose-cancel ()
   "Kill ‘undo-propose’ buffer without copying back to its parent."
